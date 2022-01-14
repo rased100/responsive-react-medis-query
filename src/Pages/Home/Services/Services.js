65d88551f62cfe359aecd7151917cb7e31c235eb
rased100 <rased100@gmail.com>
2022-01-14 23:45:32 +0600

    first commit

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const services = [
+    { name: 'Rplace Tire', price: 2000, time: 2, img: 'https://ibb.co/QcR8tdB' },
+    { name: 'Rplace Tire', price: 2000, time: 2, img: 'https://ibb.co/gJpv0pS' },
+    { name: 'Rplace Tire', price: 2000, time: 2, img: 'https://ibb.co/rwkmWpH' },
+    { name: 'Rplace Tire', price: 2000, time: 2, img: 'https://ibb.co/yVNyGn1' },
+    { name: 'Rplace Tire', price: 2000, time: 2, img: 'https://ibb.co/HhPPLbV' },
+    { name: 'Rplace Tire', price: 2000, time: 2, img: 'https://ibb.co/n37d244' },
+]
+
+const Services = () => {
+    return (
+        <div>
+            <h2>This is Service</h2>
+        </div>
+    );
+};
+
+export default Services;
